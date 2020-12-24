750f6d11616f065471a3455bf972af0eb174ae7a
TERADA-DANTE <terada.dante@gmail.com>
2020-12-24 15:48:46 +0900

    Update app.module.ts
    ❌ delete initial controller, service, and specs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  // Controllers get url and execute its function which inherits from service.
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

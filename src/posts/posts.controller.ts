39584f4005cf077a67e58106fdabadf87af119ac
TERADA-DANTE <terada.dante@gmail.com>
2020-12-26 23:27:45 +0900

    Update posts.controller.ts
    - Linting

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,18 +1,12 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { Result } from './enitities/result.entity';
 import { ResultDto } from './posts.dto';
-// import { PostsService } from './posts.service';
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly prismaService: PrismaService) {}
   // Naming convention
 
-  // @Get('/:zipcode')
-  // //getAddressById
-  // getAddress(@Param('zipcode') zipcode: string): Result {
-  //   return this.postsService.getAddress(zipcode);
-  // }
   @Get()
   findAll(): Promise<ResultDto[]> {
     return this.prismaService.result.findMany();

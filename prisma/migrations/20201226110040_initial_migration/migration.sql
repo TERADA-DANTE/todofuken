9bc1fcc08e6355cd7dcc599f3b9c7fd05d1e76c4
TERADA-DANTE <terada.dante@gmail.com>
2020-12-26 21:00:17 +0900

    Reset database

--- prisma/migrations/20201226110040_initial_migration/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Result" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "zipcode" TEXT NOT NULL,
-    "prefecture" TEXT NOT NULL,
-    "city" TEXT NOT NULL,
-    "area" TEXT NOT NULL,
-    "code" INTEGER NOT NULL
-);

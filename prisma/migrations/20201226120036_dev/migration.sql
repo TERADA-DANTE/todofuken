60d11dc689658b00e41107c912917815e2daca38
TERADA-DANTE <terada.dante@gmail.com>
2020-12-26 21:00:48 +0900

    Migrate database
    - npx prisma migrate dev --preview-feature

---
+++ prisma/migrations/20201226120036_dev/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Result" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "zipcode" TEXT NOT NULL,
+    "prefecture" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "area" TEXT NOT NULL,
+    "code" INTEGER NOT NULL
+);

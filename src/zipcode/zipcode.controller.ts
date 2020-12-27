754ec788c739264e4c623e2f5b4d59ca02606a66
TERADA-DANTE <terada.dante@gmail.com>
2020-12-27 12:47:59 +0900

    Update spec files

--- src/zipcode/zipcode.controller.ts
+++ src/zipcode/zipcode.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { ResultDto } from './dto/zipcode.dto';
 
 @Controller('zipcode')

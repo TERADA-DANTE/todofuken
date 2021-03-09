8445c35d1d097b9573ee450c91feb9eb3b83e6fd
Matsunaga syun <terada.dante@gmail.com>
2021-03-09 10:07:15 +0900

    :heavy_plus_sign: Add bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+// Components
+import Head from "next/head";
+
+// Styles
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
